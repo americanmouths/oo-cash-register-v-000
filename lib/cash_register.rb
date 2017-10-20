db87bb32a73742b76dde39952794944368032dde
Kayla Hansen <shiverr@gmail.com>
2017-10-20 01:19:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,7 @@ class CashRegister
   def add_item(title, price, quantity=1)
     if quantity > 0
     self.total += (price * quantity)
+    @items << title
   else
     self.total += price
     @items << title
